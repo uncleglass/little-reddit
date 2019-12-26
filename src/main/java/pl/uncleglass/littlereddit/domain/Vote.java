00ecffdd2d4b3f04ced070bb6d8bbcc8cfe3ca5e
Bartłomiej Kiliszek <bartekkiliszek@gmail.com>
2019-12-26 13:23:36 +0100

    Added Vote entity

---
+++ src/main/java/pl/uncleglass/littlereddit/domain/Vote.java
@@ -0,0 +1,27 @@
+package pl.uncleglass.littlereddit.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class Vote extends Auditable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NonNull
+    private short direction;
+
+    @NonNull
+    @ManyToOne
+    private Link link;
+}
