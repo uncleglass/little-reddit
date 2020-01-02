c2501e2b26bc5c688bc88e79e294d80a371c0b16
Bartłomiej Kiliszek <bartekkiliszek@gmail.com>
2020-01-02 21:56:13 +0100

    Moved beans to configuration file

--- src/main/java/pl/uncleglass/littlereddit/LittleRedditApplication.java
+++ src/main/java/pl/uncleglass/littlereddit/LittleRedditApplication.java
@@ -1,28 +1,12 @@
 package pl.uncleglass.littlereddit;
 
-import org.ocpsoft.prettytime.PrettyTime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 
 @SpringBootApplication
 public class LittleRedditApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LittleRedditApplication.class, args);
-    }
-
-    @Bean
-    PrettyTime getPrettyTime() {//TODO move it to separate configuration file
-        return new PrettyTime();
-    }
-
-    // TODO * Configuring this bean should not be needed once Spring Boot's Thymeleaf starter includes configuration
-// TODO   for thymeleaf-extras-springsecurity5 (instead of thymeleaf-extras-springsecurity4)
-    @Bean
-    public SpringSecurityDialect securityDialect() {
-        return new SpringSecurityDialect();
     }
 }
--- src/main/java/pl/uncleglass/littlereddit/configuration/Config.java
+++ src/main/java/pl/uncleglass/littlereddit/configuration/Config.java
@@ -1,8 +1,10 @@
 package pl.uncleglass.littlereddit.configuration;
 
 import org.modelmapper.ModelMapper;
+import org.ocpsoft.prettytime.PrettyTime;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 
 @Configuration
 public class Config {
@@ -10,5 +12,15 @@ public class Config {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    PrettyTime prettyTime() {
+        return new PrettyTime();
+    }
+
+    @Bean
+    public SpringSecurityDialect securityDialect() {
+        return new SpringSecurityDialect();
     }
 }
