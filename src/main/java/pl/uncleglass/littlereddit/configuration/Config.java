156e198f0475a25f041f0a19d6b1e86e277e9582
Bartłomiej Kiliszek <bartekkiliszek@gmail.com>
2020-01-02 21:52:25 +0100

    Added ModelMapper bean

--- src/main/java/pl/uncleglass/littlereddit/configuration/Config.java
+++ src/main/java/pl/uncleglass/littlereddit/configuration/Config.java
@@ -1,7 +1,14 @@
 package pl.uncleglass.littlereddit.configuration;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Config {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
