2bda35ff330063179263bd92e5bd74aff28c5945
Bartłomiej Kiliszek <bartekkiliszek@gmail.com>
2020-01-02 22:20:40 +0100

    Added mapToLink method to Mapper
    Method should map dto LinkRequest object to Link entity object

--- src/main/java/pl/uncleglass/littlereddit/services/Mapper.java
+++ src/main/java/pl/uncleglass/littlereddit/services/Mapper.java
@@ -1,4 +1,9 @@
 package pl.uncleglass.littlereddit.services;
 
+import pl.uncleglass.littlereddit.domain.Link;
+import pl.uncleglass.littlereddit.domain.dtos.LinkRequest;
+
 public interface Mapper {
+
+    Link mapToLink(LinkRequest linkRequest);
 }
--- src/main/java/pl/uncleglass/littlereddit/services/MapperImpl.java
+++ src/main/java/pl/uncleglass/littlereddit/services/MapperImpl.java
@@ -2,6 +2,8 @@ package pl.uncleglass.littlereddit.services;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import pl.uncleglass.littlereddit.domain.Link;
+import pl.uncleglass.littlereddit.domain.dtos.LinkRequest;
 
 @Service
 public class MapperImpl implements Mapper {
@@ -12,4 +14,7 @@ public class MapperImpl implements Mapper {
         this.modelMapper = modelMapper;
     }
 
+    public Link mapToLink(LinkRequest linkRequest) {
+        return modelMapper.map(linkRequest, Link.class);
+    }
 }
